a750585842c5538023f154257bd561638b0b901b
tanel <taneli.juga@metropolia.fi>
2021-10-25 07:10:15 +0300

    Updated backend to use mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
+const Contact = require('./models/contact')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+
+const errorHandler = (error, request, response, next) => {
+    //console.log('error: ', error)
+    console.log('error.name: ', error.name)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }else if(error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
@@ -13,191 +31,63 @@ app.use(cors())
 app.use(express.json())
 app.use(logger)
 
-function isString(value){
-    return typeof value === 'string';
-}
-
-function notEmptyString(value){
-    return value.length > 0;
-}
-
-const fields = [
-    [
-        "name",
-        [isString, notEmptyString]
-    ], 
-    [
-        "number",
-        [isString, notEmptyString]
-    ]
-].map(([key, test, defaultValue]) => {
-    return {
-        key,
-        test,
-        defaultValue
-    }
-})
-.map((field) => {
-    const {test} = field;
-    let newTest = test;
-    if(Array.isArray(test)){
-        newTest = (value) => {
-            return test.reduce((solution, cur) => {
-                return solution && cur(value);
-            }, true)
-        }
-    }
-    return {
-        ...field,
-        test: newTest
-    }
-})
-.map(({key, test, defaultValue}) => {
-    const hasDefault = defaultValue !== undefined;
-    const newTest = (value) => {
-        const notDefined = value === undefined;
-        if(hasDefault){
-            if(notDefined){
-                return defaultValue;
-            }  
-        }else {
-            if(notDefined){
-                throw {
-                    message: `${key} missing`,
-                    status: 400
-                }
-            }  
-        }
-        if(!test(value)){
-            throw {
-                message: `invalid ${key}`,
-                status: 400
-            }
-        }
-        console.log('test passed!');
-        return value;
-    }
-    return {key, test: newTest}
+app.get('/api/contacts', (request, response) => {
+    Contact.find({}).then(result => {
+        response.json(result)
+    })
 })
 
-
-let data = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-]
-
-function makeItem(item){
-    return fields.reduce( (obj, {key, test}) => {
-        obj[key] = test(item[key]);
-        return obj
-    }, {})
-}
-
-function generateId(){
-    const maxId = data.length > 0
-    ? Math.max(...data.map(e => e.id))
-    : 0
-    return maxId + 1
-}
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/contacts', (req, res) => {
-    res.json(data)
-})
-
-app.get('/api/contacts/:id', (request, response) => {
+app.get('/api/contacts/:id', (request, response, next) => {
     const id = request.params.id
-    //console.log(id)
-    const item = data.find(item => item.id == id)
-    
-    //console.log(item)
-    
-    if(item){
-        response.json(item)
-    }else{
-        response.status(404).end()
-    }
+    Contact.findById(id)
+    .then(result => {
+        if(result){
+            response.json(result)
+        }else{
+            response.status(500).end()
+        }
+    })
+    .catch(error => {
+        next(error)
+    })
 })
 
 app.delete('/api/contacts/:id', (request, response) => {
     const id = request.params.id
-    data = data.filter(item => item.id != id)
-
-    response.status(204).end()
+    Contact.findByIdAndRemove(id)
+    .then(result => {
+        response.json(result)
+    })
 })
 
-app.post('/api/contacts', (request, response) => {
-    const item = makeItem(request.body)
+app.post('/api/contacts', (request, response, next) => {
+    const {name, number} = request.body
 
-    item.id = generateId()
-    item.date = Date.now()
-
-    data = data.concat(item)
-    console.log(data)
-
-    response.json(item)
+    const item = new Contact({
+      name: name,
+      number: number,
+      date: new Date(),
+    })
+  
+    item.save().then(savedItem => {
+      response.json(savedItem)
+    }).catch(error => {
+        next(error)
+    })
 })
 
 app.put('/api/contacts/:id', (request, response) => {
-    const id = request.params.id 
-    const oldItem = data.find(item => item.id == id);
+    const id = request.params.id
+    const {name, number} = request.body
 
-    if(!oldItem){
-        return response.status(404).json({ 
-            error: 'no contact with that id' 
-        })
-    }
 
-    const newItem = makeItem(request.body)
-    newItem.id = id
-    newItem.date = Date.now()
-
-    console.log("newItem: ", newItem)
-    data = data.map(item => {
-        if(item.id == id){
-            return newItem;
-        }
-        return item;
-    })
-    console.log(data)
-
-    response.json(newItem)
-})
-
-app.use(function (err, req, res, next) {
-    console.log('in error handler')
-    console.log('err.status ', err.status)
-    console.log(err)
-    const status = err.status ? err.status : 500;
-    res.status(status).send({
-        message: err.message
+    Contact.findByIdAndUpdate(id, {name, number}, {new: true})
+    .then(result => {
+        response.json(result)
     })
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unkown endpoint'})
-}
+app.use(errorHandler)
 
 app.use(unknownEndpoint)
 
---
+++ backend/models/contact.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 1,
+    required: true
+  },
+  number: String,
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+schema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Contact', schema)
